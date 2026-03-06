6765cb97bd4245a1a323094373a74f119f14cbd2
Gabo173 <galvansantanag@gmail.com>
2026-03-05 19:59:33 -0500

    Add files via upload

---
+++ php/imc.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Programacion</title>
+    <style>
+        
+        body {
+            margin: 0;
+        }
+        header {
+            padding: 30px;
+            text-align: center;
+            background-color: aliceblue;
+        }
+    </style>
+
+</head>
+<body>
+<header>
+    <h1 style="color:#2078c9;">como te pusiste esos jeanes</h1>
+    <a href="index.php">Home</a>
+    <a href="contacto.php">Contacto.php</a>
+
+    <p></p>
+    <p></p>
+
+</body>
+</html>
